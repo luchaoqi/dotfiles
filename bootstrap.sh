9e53f7a6fabc3de72020dcecd943bfacfcdf38f5
Luchao Qi <46330265+luchaoqi@users.noreply.github.com>
2023-10-22 11:06:10 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -125,6 +125,8 @@ if [ -x "$(command -v zsh)" ]; then
     git clone https://github.com/jirutka/zsh-shift-select.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-shift-select
     git clone https://github.com/esc/conda-zsh-completion ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/conda-zsh-completion
     git clone https://github.com/wbingli/zsh-wakatime.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/wakatime
+
+    git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autoupdate
 fi
 
 cp ~/dotfiles/.zshrc ~
